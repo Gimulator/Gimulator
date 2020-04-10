cb9be57924f8e520b81e50859795455b70fd78ca
Ali Soume'e <alidadar7676@gmail.com>
2020-04-10 11:38:14 +0430

    Add Spreader for watch operation

---
+++ spread/spreader.go
@@ -0,0 +1,44 @@
+package spread
+
+import (
+	"sync"
+
+	"gitlab.com/Syfract/Xerac/gimulator/object"
+)
+
+type watcher struct {
+	key object.Key
+	ch  chan object.Object
+}
+
+type Spreader struct {
+	sync.Mutex
+	watchers []watcher
+}
+
+func (s *Spreader) AddWatcher(key object.Key, ch chan object.Object) {
+	s.Lock()
+	defer s.Unlock()
+
+	watcher := watcher{
+		key: key,
+		ch:  ch,
+	}
+
+	s.watchers = append(s.watchers, watcher)
+}
+
+func (s *Spreader) Spread(obj object.Object) {
+	s.Lock()
+	defer s.Unlock()
+
+	key := obj.Key
+	for _, w := range s.watchers {
+		if w.key.Match(key) {
+			select {
+			case w.ch <- obj:
+			default:
+			}
+		}
+	}
+}
