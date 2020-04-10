1762ae081a9510605eb47d7eb41d03998232475e
Ali Soume'e <alidadar7676@gmail.com>
2020-04-10 09:25:45 +0430

    Define key and object structs

---
+++ object/object.go
@@ -0,0 +1,34 @@
+package object
+
+type Key struct {
+	Type      string
+	Namespace string
+	Name      string
+}
+
+func (k *Key) Equal(key *Key) bool {
+	if k.Type != key.Type {
+		return false
+	} else if k.Namespace != key.Namespace {
+		return false
+	} else if k.Name != key.Name {
+		return false
+	}
+	return true
+}
+
+func (k *Key) Match(key *Key) bool {
+	if k.Type != "" && k.Type != key.Type {
+		return false
+	} else if k.Namespace != "" && k.Namespace != key.Namespace {
+		return false
+	} else if k.Name != "" && k.Name != key.Name {
+		return false
+	}
+	return true
+}
+
+type Object struct {
+	Key   Key
+	Value interface{}
+}
