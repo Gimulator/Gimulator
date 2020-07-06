05785ce36c853ca53d4e7c584ca8e2d018a6ce10
Amir Aminbeidokhti <amir.aminb97@gmail.com>
2020-07-06 14:05:59 +0430

    func TestKeyString, func TestEqual added to object_test file

---
+++ object/object_test.go
@@ -0,0 +1,51 @@
+package object
+
+import (
+	"testing"
+)
+
+func TestKeyString(t *testing.T) {
+
+	var tests = []struct {
+		key  Key
+		want string
+	}{
+		{Key{"Type", "Namespace", "Name"}, "{Type: Type, Namespace: Namespace, Name: Name}"},
+		{Key{"", "", ""}, "{Type: , Namespace: , Name: }"},
+		{Key{}, "{Type: , Namespace: , Name: }"},
+		{Key{Type: "t"}, "{Type: t, Namespace: , Name: }"},
+	}
+
+	for _, test := range tests {
+
+		got := test.key.String()
+
+		if got != test.want {
+			t.Errorf("got %s, want %s", got, test.want)
+		}
+	}
+}
+
+func TestEqual(t *testing.T) {
+
+	var tests = []struct {
+		key1 Key
+		key2 *Key
+		want bool
+	}{
+		{Key{"Type", "Namespace", "Name"}, &Key{"Type", "Namespace", "Name"}, true},
+		{Key{"", "", ""}, &Key{}, true},
+		{Key{Type: "t"}, &Key{Type: "t"}, true},
+		{Key{"type", "namespace", "name"}, &Key{"Type", "Namespace", "Name"}, false},
+		{Key{"tpe", "namespace", "name"}, &Key{"Type", "Namespace", "Name"}, false},
+	}
+
+	for _, test := range tests {
+
+		got := test.key1.Equal(test.key2)
+
+		if got != test.want {
+			t.Errorf("got %v, want %v", got, test.want)
+		}
+	}
+}
