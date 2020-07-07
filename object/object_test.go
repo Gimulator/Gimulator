5ff6dc79f543af720fe150030a9ec1bfb2e96193
Amir Aminbeidokhti <amir.aminb97@gmail.com>
2020-07-07 14:46:19 +0430

    Define package level keys and add documentation. object_test.go

--- object/object_test.go
+++ object/object_test.go
@@ -1,27 +1,48 @@
 package object
 
 import (
+	"fmt"
 	"testing"
 )
 
-func TestKeyString(t *testing.T) {
+const checkMark = "\u2713"
+const ballotX = "\u2717"
+
+func LogApproved(want interface{}, checkMark string) string {
+	return fmt.Sprintf("\t\tShould have a \"%v\" %v", want, checkMark)
+}
+
+func LogFailed(got, want interface{}, ballotX string) string {
+	return fmt.Sprintf("\t\tgot %v ***** want %v %v", got, want, ballotX)
+}
+
+func TestKeyStringer(t *testing.T) {
 
 	var tests = []struct {
 		key  Key
 		want string
 	}{
-		{Key{"Type", "Namespace", "Name"}, "{Type: Type, Namespace: Namespace, Name: Name}"},
-		{Key{"", "", ""}, "{Type: , Namespace: , Name: }"},
-		{Key{}, "{Type: , Namespace: , Name: }"},
-		{Key{Type: "t"}, "{Type: t, Namespace: , Name: }"},
+		{KeyComplete, "{Type: t, Namespace: ns, Name: n}"},
+		{KeyEmpty, "{Type: , Namespace: , Name: }"},
+		{KeyOnlyType, "{Type: t, Namespace: , Name: }"},
+		{KeyOnlyNamespace, "{Type: , Namespace: ns, Name: }"},
+		{KeyOnlyName, "{Type: , Namespace: , Name: n}"},
+		{KeyTypeNamespace, "{Type: t, Namespace: ns, Name: }"},
+		{KeyTypeName, "{Type: t, Namespace: , Name: n}"},
+		{KeyNamespaceName, "{Type: , Namespace: ns, Name: n}"},
 	}
 
+	t.Log("Given the need to test method String of Key type.")
+
 	for _, test := range tests {
+		t.Logf("\tWhen checking \"%v\"", test.key)
 
 		got := test.key.String()
 
 		if got != test.want {
-			t.Errorf("got %s, want %s", got, test.want)
+			t.Errorf(LogFailed(got, test.want, ballotX))
+		}else{
+			t.Logf(LogApproved(test.want, checkMark))	
 		}
 	}
 }
@@ -33,21 +54,24 @@ func TestEqual(t *testing.T) {
 		key2 *Key
 		want bool
 	}{
-		{Key{"Type", "Namespace", "Name"}, &Key{"Type", "Namespace", "Name"}, true},
-		{Key{"", "", ""}, &Key{}, true},
-		{Key{Type: "t"}, &Key{Type: "t"}, true},
-		{Key{"type", "namespace", "name"}, &Key{"Type", "Namespace", "Name"}, false},
-		{Key{"tpe", "namespace", "name"}, &Key{"Type", "Namespace", "Name"}, false},
-		{Key{"t", "ns", "name"}, &Key{"t", "Namespace", "Name"}, false},
-		{Key{"t", "ns", "n"}, &Key{"t", "ns", "Name"}, false},
+		{KeyEmpty, &Key{}, true},
+		{KeyOnlyType, &Key{Type: "t"}, true},
+		{KeyComplete, &Key{"t", "ns", "n"}, true},
+		{KeyComplete, &Key{"T", "Ns", "N"}, false},
+		{KeyOnlyName, &Key{"t", "Namespace", "Name"}, false},
 	}
 
+	t.Log("Given the need to test method Equal of Key type.")
+
 	for _, test := range tests {
+		t.Logf("\tWhen comparing \"%v\" to \"%v\"", test.key1, test.key2)
 
 		got := test.key1.Equal(test.key2)
 
 		if got != test.want {
-			t.Errorf("got %v, want %v", got, test.want)
+			t.Errorf(LogFailed(got, test.want, ballotX))
+		}else{
+			t.Logf(LogApproved(test.want, checkMark))	
 		}
 	}
 }
@@ -59,47 +83,66 @@ func TestMatch(t *testing.T) {
 		key2 *Key
 		want bool
 	}{
-		{Key{"Type", "Namespace", "Name"}, &Key{"Type", "Namespace", "Name"}, true},
-		{Key{"", "", ""}, &Key{}, true},
-		{Key{Type: "t"}, &Key{Type: "t"}, true},
-		{Key{"type", "namespace", "name"}, &Key{"Type", "Namespace", "Name"}, false},
-		{Key{"tpe", "namespace", "name"}, &Key{"Type", "Namespace", "Name"}, false},
-		{Key{Type: "t"}, &Key{"t", "a", "b"}, true},
-		{Key{"Type", "Namespace", "Name"}, &Key{Type: "Type"}, false},
-		{Key{}, &Key{}, true},
-		{Key{}, &Key{"a", "b", "c"}, true},
-		{Key{Type: "type"}, &Key{Type: "type", Namespace: "ns"}, true},
-		{Key{"Type", "ns", "n"}, &Key{"Type", "ns", "name"}, false},
+		{KeyComplete, &Key{"t", "ns", "n"}, true},
+		{KeyOnlyType, &Key{Type: "t"}, true},
+		{KeyOnlyType, &Key{"t", "a", "b"}, true},
+		{KeyEmpty, &Key{}, true},
+		{KeyEmpty, &Key{"a", "b", "c"}, true},
+		{KeyOnlyType, &KeyTypeNamespace, true},
+		{KeyTypeNamespace, &KeyOnlyName, false},
+		{KeyComplete, &Key{"T", "Ns", "N"}, false},
+		{KeyComplete, &KeyOnlyType, false},
 	}	
 
+	t.Log("Given the needed to test mehod Match of Key type.")
+
 	for _, test := range tests {
+		t.Logf("\tWhen comparing \"%v\" to \"%v\"", test.key1, test.key2)
 
 		got := test.key1.Match(test.key2)
 
 		if got != test.want {
-			t.Errorf("got %v, want %v", got, test.want)
+			t.Errorf(LogFailed(got, test.want, ballotX))
+		} else {
+			t.Logf(LogApproved(test.want, checkMark))
 		}
 	}
 }
 
-func TestObjectString(t *testing.T) {
+func TestObjectStringer(t *testing.T) {
 
 	var tests = []struct {
 		o    Object
 		want string
 	}{
-		{Object{Key: &Key{Type: "Type"}, Value: "Value"}, "{Key: {Type: Type, Namespace: , Name: }, Value: 'Value'}"},
-		{Object{Key: &Key{"t", "ns", "n"}, Value: "thisisatestvalue"}, "{Key: {Type: t, Namespace: ns, Name: n}, Value: 'thisisates...'}"},
+		{Object{Key: &KeyOnlyType, Value: "Value"}, "{Key: {Type: t, Namespace: , Name: }, Value: 'Value'}"},
+		{Object{Key: &KeyComplete, Value: "thisisatestvalue"}, "{Key: {Type: t, Namespace: ns, Name: n}, Value: 'thisisates...'}"},
 		{Object{}, "{Key: <nil>, Value: ''}"},
-		{Object{Key: &Key{Name: "n"}, Value: ""}, "{Key: {Type: , Namespace: , Name: n}, Value: ''}"},
+		{Object{Key: &KeyOnlyName, Value: ""}, "{Key: {Type: , Namespace: , Name: n}, Value: ''}"},
 	}
 
+	t.Log("Given the needed to test method String of Object type.")
+
 	for _, test := range tests {
+		t.Logf("\tWhen checking \"%v\"", test.o)
 
 		got := test.o.String()
 
 		if got != test.want {
-			t.Errorf("got %v, want %v", got, test.want)
+			t.Errorf(LogFailed(got, test.want, ballotX))
+		}else{
+			t.Logf(LogApproved(test.want, checkMark))
 		}
 	}
 }
+
+var (
+	KeyComplete = Key{"t", "ns", "n"}
+	KeyEmpty = Key{}
+	KeyOnlyType = Key{Type: "t"}
+	KeyOnlyNamespace = Key{Namespace : "ns"}
+	KeyOnlyName = Key{Name : "n"}
+	KeyTypeNamespace = Key{Type : "t", Namespace : "ns"}
+	KeyTypeName = Key{Type : "t", Name : "n"}
+	KeyNamespaceName = Key{Namespace : "ns", Name : "n"}
+)
