8c9ceb605118be083ce919b7481b1e89b30c667b
Amir Aminbeidokhti <amir.aminb97@gmail.com>
2020-07-14 20:37:18 +0430

    TestSet method was added to simulator_test.go file.

--- simulator/simulator_test.go
+++ simulator/simulator_test.go
@@ -61,3 +61,72 @@ func TestGet(t *testing.T) {
 	}
 
 }
+
+func TestSet(t *testing.T) {
+	strg := storage.NewMemory()
+	strg.Set(&ObjectKComplete)
+	s := &Simulator{
+		Mutex:    sync.Mutex{},
+		spreader: NewSpreader(),
+		storage:  strg,
+	}
+
+	tests := []struct {
+		id string
+		obj *object.Object
+		key *object.Key
+		wantObj *object.Object
+		wantErr error
+	}{
+		{"id1", &ObjectKComplete, &KeyComplete, &ObjectKComplete, nil},
+		{"id2", &ObjectKEmpty, &KeyEmpty, nil, fmt.Errorf("error")},
+	}
+
+	t.Logf("Given the need to test Set method of Simulator type.")
+
+	for _, test := range tests {
+		t.Logf("\tWhen checking the value \"%v, %v\"", test.key, test.id)
+
+		gotErr := s.Set(test.id, test.obj)
+		gotObj, _ := s.storage.Get(test.key)
+
+		if reflect.DeepEqual(gotObj, test.wantObj) && reflect.TypeOf(gotErr) == reflect.TypeOf(test.wantErr) {
+			t.Logf(LogApproved(test.wantObj, checkMark))
+		} else if reflect.TypeOf(gotErr) != reflect.TypeOf(test.wantErr) {
+			t.Errorf(LogFailed(gotErr, test.wantErr, ballotX))
+		} else {
+			t.Errorf(LogFailed(gotObj, test.wantObj, ballotX))
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
