baef445f0b2c9c5a94e0171eb5417e395ae02e06
Amir Aminbeidokhti <amir.aminb97@gmail.com>
2020-07-14 18:50:01 +0430

    TestAddWatcher was added to spreader_test.go file.

--- simulator/spreader_test.go
+++ simulator/spreader_test.go
@@ -1,17 +1,17 @@
 package simulator
 
 import (
-	_ "fmt"
-	"testing"
-	_ "github.com/Gimulator/Gimulator/object"
+	"fmt"
+	"github.com/Gimulator/Gimulator/object"
 	"github.com/sirupsen/logrus"
 	"reflect"
+	"testing"
 )
 
 func TestNewSpreader(t *testing.T) {
-	want := &spreader {
-		watchers : make(map[string]watcher),
-		log : logrus.WithField("entity", "spreader"),
+	want := &spreader{
+		watchers: make(map[string]watcher),
+		log:      logrus.WithField("entity", "spreader"),
 	}
 
 	t.Logf("Given the need to test NewSpreader function of watcher type.")
@@ -25,3 +25,44 @@ func TestNewSpreader(t *testing.T) {
 	}
 }
 
+func TestAddWatcher(t *testing.T) {
+	s := &spreader{
+		watchers: make(map[string]watcher),
+		log:      logrus.WithField("entity", "spreader"),
+	}
+	s.watchers["test"] = watcher{
+		keys: []*object.Key{&KeyComplete, &KeyOnlyType},
+		ch:   make(chan *object.Object),
+	}
+
+	testCh := make(chan *object.Object)
+	var tests = []struct {
+		id          string
+		key         *object.Key
+		ch          chan *object.Object
+		wantErr     error
+		wantWatcher watcher
+	}{
+		{"test", &KeyOnlyName, nil, nil, s.watchers["test"]},
+		{"test", &KeyOnlyName, make(chan *object.Object), nil, s.watchers["test"]},
+		{"test1", &KeyOnlyType, nil, fmt.Errorf("nil channel for creating new watcher"), watcher{}},
+		{"test2", &KeyOnlyType, testCh, nil, watcher{[]*object.Key{&KeyOnlyType}, testCh}},
+	}
+
+	t.Logf("Given the need to test addWatcher method of spreader type.")
+
+	for _, test := range tests {
+		t.Logf("\tWhen checking the value \"%v, %v, %v\"", test.id, test.key, test.ch)
+
+		gotErr := s.AddWatcher(test.id, test.key, test.ch)
+		gotWatcher := s.watchers[test.id]
+
+		if reflect.DeepEqual(gotErr, test.wantErr) && reflect.DeepEqual(gotWatcher, test.wantWatcher) {
+			t.Logf(LogApproved(test.wantWatcher, checkMark))
+		} else if !reflect.DeepEqual(gotErr, test.wantErr) {
+			t.Errorf(LogFailed(gotErr, test.wantErr, ballotX))
+		} else {
+			t.Errorf(LogFailed(gotWatcher, test.wantWatcher, ballotX))
+		}
+	}
+}
