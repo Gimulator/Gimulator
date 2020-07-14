f0fdf739c16735dd5bea206651254e682aed15f0
Amir Aminbeidokhti <amir.aminb97@gmail.com>
2020-07-14 19:58:07 +0430

    TestGet method was added to simulator_test.go file.

--- simulator/simulator_test.go
+++ simulator/simulator_test.go
@@ -1,19 +1,20 @@
 package simulator
 
-import(
-	_ "github.com/Gimulator/Gimulator/object"
+import (
+	"fmt"
+	"github.com/Gimulator/Gimulator/object"
 	"github.com/Gimulator/Gimulator/storage"
 	"reflect"
-	"testing"
 	"sync"
+	"testing"
 )
 
 func TestNewSimulator(t *testing.T) {
 	strg := storage.NewMemory()
 	want := &Simulator{
-		Mutex: sync.Mutex{},
+		Mutex:    sync.Mutex{},
 		spreader: NewSpreader(),
-		storage: strg,
+		storage:  strg,
 	}
 
 	got := NewSimulator(strg)
@@ -23,4 +24,40 @@ func TestNewSimulator(t *testing.T) {
 	} else {
 		t.Errorf(LogFailed(got, want, ballotX))
 	}
+}
+
+func TestGet(t *testing.T) {
+	strg := storage.NewMemory()
+	strg.Set(&ObjectKComplete)
+	s := &Simulator{
+		Mutex:    sync.Mutex{},
+		spreader: NewSpreader(),
+		storage:  strg,
+	}
+
+	tests := []struct {
+		key     *object.Key
+		id      string
+		wantObj *object.Object
+		wantErr error
+	}{
+		{&KeyComplete, "test id", &ObjectKComplete, nil},
+		{&KeyEmpty, "another id", nil, fmt.Errorf("error")},
+	}
+
+	t.Logf("Given the need to test Get method of Simulator type.")
+
+	for _, test := range tests {
+		t.Logf("\tWhen checking the value \"%v, %v\"", test.key, test.id)
+		gotObj, gotErr := s.Get(test.id, test.key)
+
+		if reflect.TypeOf(gotErr) == reflect.TypeOf(test.wantErr) && reflect.DeepEqual(gotObj, test.wantObj) {
+			t.Logf(LogApproved(test.wantObj, checkMark))
+		} else if !reflect.DeepEqual(gotErr, test.wantErr) {
+			t.Errorf(LogFailed(gotErr, test.wantErr, ballotX))
+		} else {
+			t.Errorf(LogFailed(gotObj, test.wantObj, ballotX))
+		}
+	}
+
 }
