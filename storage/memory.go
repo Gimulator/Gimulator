3d24b9a1d3505d58ea4359b0ee41b383c8140522
MaryamHS <m.hajishah75@gmail.com>
2020-11-02 14:23:35 +0330

    updated memory with protobuf based interface,  ValidateKey function deleted.This file is not tested

--- storage/memory.go
+++ storage/memory.go
@@ -3,89 +3,92 @@ package storage
 import (
 	"fmt"
 
-	"github.com/Gimulator/Gimulator/object"
+	"github.com/Gimulator/protobuf/go/api"
 )
 
+type Identifier struct {
+	Type      string
+	Name      string
+	Namespace string
+}
+
+//Casts a key type to an identifier type
+func KeyToiden(key *api.Key) *Identifier {
+	return &Identifier{
+		Type:      key.Type,
+		Name:      key.Name,
+		Namespace: key.Namespace,
+	}
+}
+
 type Memory struct {
-	storage map[object.Key]*object.Object
+	storage map[Identifier]*api.Message
 }
 
 func NewMemory() *Memory {
 	return &Memory{
-		storage: make(map[object.Key]*object.Object),
+		storage: make(map[Identifier]*api.Message),
 	}
 }
 
-func (m *Memory) Get(key *object.Key) (*object.Object, error) {
-	return m.get(key)
+func (m *Memory) Get(key *api.Key) (*api.Message, error) {
+	iden := KeyToiden(key)
+	return m.get(iden)
 }
 
-func (m *Memory) Set(obj *object.Object) error {
-	return m.set(obj)
+func (m *Memory) Put(msg *api.Message) error {
+	return m.put(msg)
 }
 
-func (m *Memory) Delete(key *object.Key) error {
-	return m.delete(key)
+func (m *Memory) Delete(key *api.Key) error {
+	iden := KeyToiden(key)
+	return m.delete(iden)
 }
 
-func (m *Memory) Find(key *object.Key) ([]*object.Object, error) {
-	return m.find(key), nil
+func (m *Memory) GetAll(key *api.Key) ([]*api.Message, error) {
+	iden := KeyToiden(key)
+	return m.getall(iden), nil
 }
 
-func (m *Memory) get(key *object.Key) (*object.Object, error) {
-	err := m.validateKey(key)
-	if err != nil {
-		return nil, err
+func (iden *Identifier) MatchKeys(key *Identifier) bool {
+	if iden.Type != "" && iden.Type != key.Type {
+		return false
+	} else if iden.Namespace != "" && iden.Namespace != key.Namespace {
+		return false
+	} else if iden.Name != "" && iden.Name != key.Name {
+		return false
 	}
-
-	if object, exists := m.storage[*key]; exists {
-		return object, nil
-	}
-	return nil, fmt.Errorf("object with key=%v does not exist", key)
+	return true
 }
 
-func (m *Memory) set(obj *object.Object) error {
-	err := m.validateKey(obj.Key)
-	if err != nil {
-		return err
+func (m *Memory) get(iden *Identifier) (*api.Message, error) {
+	if msg, exists := m.storage[*iden]; exists {
+		return msg, nil
 	}
+	return nil, fmt.Errorf("object message with key=%v does not exist", iden)
+}
 
-	m.storage[*obj.Key] = obj
+//puts a message in storage
+func (m *Memory) put(msg *api.Message) error {
+	iden := KeyToiden(msg.Key)
+	m.storage[*iden] = msg
 	return nil
 }
 
-func (m *Memory) delete(key *object.Key) error {
-	err := m.validateKey(key)
-	if err != nil {
-		return err
-	}
-
-	if _, exists := m.storage[*key]; exists {
-		delete(m.storage, *key)
+func (m *Memory) delete(iden *Identifier) error {
+	if _, exists := m.storage[*iden]; exists {
+		delete(m.storage, *iden)
 		return nil
 	}
-	return fmt.Errorf("object with key=%v does not exist", key)
+	return fmt.Errorf("message object with key=%v does not exist", iden)
 }
 
-func (m *Memory) find(key *object.Key) []*object.Object {
-	result := make([]*object.Object, 0)
-	for k, o := range m.storage {
-		if key.Match(&k) {
+func (m *Memory) getall(iden *Identifier) []*api.Message {
+	result := make([]*api.Message, 0)
+	for i, o := range m.storage {
+		if iden.MatchKeys(&i) {
 			result = append(result, o)
 		}
 	}
 	return result
-}
-
-func (m *Memory) validateKey(key *object.Key) error {
-	if key.Name == "" {
-		return fmt.Errorf("invalid key with empty Name")
-	}
-	if key.Namespace == "" {
-		return fmt.Errorf("invalid key with empty Namespace")
-	}
-	if key.Type == "" {
-		return fmt.Errorf("invalid key with empty Type")
-	}
-	return nil
 }
