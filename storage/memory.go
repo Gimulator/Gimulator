0d43fce751cb26d3982a802316ef6e39601c32ec
unknown <mhajishah96@gmail.com>
2020-11-04 06:58:49 -0800

    errors updated with grpc status,functions are not tested

--- storage/memory.go
+++ storage/memory.go
@@ -4,64 +4,45 @@ import (
 	"fmt"
 
 	"github.com/Gimulator/protobuf/go/api"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-type Identifier struct {
-	Type      string
-	Name      string
-	Namespace string
+type identifier struct {
+	theType   string
+	name      string
+	namespace string
 }
 
 //Casts a key type to an identifier type
-func KeyToiden(key *api.Key) *Identifier {
-	return &Identifier{
-		Type:      key.Type,
-		Name:      key.Name,
-		Namespace: key.Namespace,
+func keyToiden(key *api.Key) *identifier {
+	return &identifier{
+		theType:   key.Type,
+		name:      key.Name,
+		namespace: key.Namespace,
 	}
 }
 
 type Memory struct {
-	storage map[Identifier]*api.Message
+	storage map[identifier]*api.Message
 }
 
 func NewMemory() *Memory {
 	return &Memory{
-		storage: make(map[Identifier]*api.Message),
+		storage: make(map[identifier]*api.Message),
 	}
 }
 
 func (m *Memory) Get(key *api.Key) (*api.Message, error) {
-	iden := KeyToiden(key)
-	return m.get(iden)
-}
-
-func (m *Memory) Put(msg *api.Message) error {
-	return m.put(msg)
-}
-
-func (m *Memory) Delete(key *api.Key) error {
-	iden := KeyToiden(key)
-	return m.delete(iden)
-}
-
-func (m *Memory) GetAll(key *api.Key) ([]*api.Message, error) {
-	iden := KeyToiden(key)
-	return m.getall(iden), nil
-}
-
-func (iden *Identifier) MatchKeys(key *Identifier) bool {
-	if iden.Type != "" && iden.Type != key.Type {
-		return false
-	} else if iden.Namespace != "" && iden.Namespace != key.Namespace {
-		return false
-	} else if iden.Name != "" && iden.Name != key.Name {
-		return false
+	iden := keyToiden(key)
+	getMsgResult, err := m.get(iden)
+	if err != nil {
+		return nil, status.Error(codes.Internal, fmt.Sprintf("could not get any message for key=%v", iden))
 	}
-	return true
+	return getMsgResult, nil
 }
 
-func (m *Memory) get(iden *Identifier) (*api.Message, error) {
+func (m *Memory) get(iden *identifier) (*api.Message, error) {
 	if msg, exists := m.storage[*iden]; exists {
 		return msg, nil
 	}
@@ -69,13 +50,30 @@ func (m *Memory) get(iden *Identifier) (*api.Message, error) {
 }
 
 //puts a message in storage
+func (m *Memory) Put(msg *api.Message) error {
+	putMsgResult := m.put(msg)
+	if putMsgResult != nil {
+		return status.Error(codes.Internal, fmt.Sprintf("could not put message=%v in storage", msg))
+	}
+	return putMsgResult
+}
+
 func (m *Memory) put(msg *api.Message) error {
-	iden := KeyToiden(msg.Key)
+	iden := keyToiden(msg.Key)
 	m.storage[*iden] = msg
 	return nil
 }
 
-func (m *Memory) delete(iden *Identifier) error {
+func (m *Memory) Delete(key *api.Key) error {
+	iden := keyToiden(key)
+	delMsgResult := m.delete(iden)
+	if delMsgResult != nil {
+		return status.Error(codes.Internal, fmt.Sprintf("could not delete message with key=%v ", iden))
+	}
+	return delMsgResult
+}
+
+func (m *Memory) delete(iden *identifier) error {
 	if _, exists := m.storage[*iden]; exists {
 		delete(m.storage, *iden)
 		return nil
@@ -83,12 +81,37 @@ func (m *Memory) delete(iden *Identifier) error {
 	return fmt.Errorf("message object with key=%v does not exist", iden)
 }
 
-func (m *Memory) getall(iden *Identifier) []*api.Message {
+func (m *Memory) GetAll(key *api.Key) ([]*api.Message, error) {
+	iden := keyToiden(key)
+	getallMsgsResult, err := m.getall(iden)
+	if err != nil {
+		return nil, status.Error(codes.Internal, fmt.Sprintf("could not getall messages with key=%v ", iden))
+	}
+	return getallMsgsResult, nil
+}
+
+func (m *Memory) getall(iden *identifier) ([]*api.Message, error) {
 	result := make([]*api.Message, 0)
 	for i, o := range m.storage {
-		if iden.MatchKeys(&i) {
+		if iden.matchKeys(&i) {
 			result = append(result, o)
 		}
 	}
-	return result
+
+	if result != nil {
+		return result, nil
+	}
+
+	return nil, fmt.Errorf("no messages matched with key=%v ", iden)
+}
+
+func (iden *identifier) matchKeys(key *identifier) bool {
+	if iden.theType != "" && iden.theType != key.theType {
+		return false
+	} else if iden.namespace != "" && iden.namespace != key.namespace {
+		return false
+	} else if iden.name != "" && iden.name != key.name {
+		return false
+	}
+	return true
 }
